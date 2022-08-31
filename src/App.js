f3528deb6231d2d4369400404b20a1d9ac3229c9
Alvin Acuña <alvindedota123@gmail.com>
2022-08-31 17:03:41 -0400

    iniciando componentes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+
+const App = () => {
+  const [proyectGrade, setProyectGrade] = useState([])
+  const [filterProyect, setFilterProyect] = useState("")
+  const [buttonSearch, setButtonSearch] = useState()
+
+  const hookServ = () => {
+    axios
+    .get('http://localhost:3001/proyecto-grado')
+    .then(response => {
+      setProyectGrade(response.data)
+    })
+  }
+
+  useEffect(hookServ, [])
+
+
+  const handleChange = (event) => {
+    setFilterProyect(event.target.value)
+  }
+
+  const addSearch = (event) => {
+    event.preventDefault()
+    console.log(event.target)
+  }
+
+  const filterGrades = proyectGrade.filter((proyect) => {
+    return proyect.title.toLowerCase().includes(filterProyect.toLowerCase()) || proyect.autor.toLowerCase().includes(filterProyect.toLowerCase())
+  })
+  return(
+    <div className="container">
+      <form className="formBusc" onSubmit={addSearch}>
+        <input className="formBusc__input" onChange={handleChange}/>
+        <button className="formBusc__button" type="submit">Buscar</button> 
+      </form>
+      <ul className="containerGrid">
+          {
+            filterGrades.map(proyectGrad => {
+              return (
+              <li key={proyectGrad.id} className="gridProyect">
+                <h3>{proyectGrad.title}</h3>
+                <p>{proyectGrad.autor}</p>
+                <p>{proyectGrad.gestion}</p>
+                <p>{proyectGrad.carrera}</p>
+              </li>
+              )
+            })
+          }
+        </ul>
     </div>
-  );
+  )
+  
 }
 
 export default App;
